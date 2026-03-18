2af3306775a6d98d7f19128ec0c0ff499e2e4272
Camille <caod0001@stud.ek.dk>
2026-03-18 16:41:46 +0100

    start js product list

--- js/productlist.js
+++ js/productlist.js
@@ -1,5 +1,5 @@
 const urlcategory = new URLSearchParams(window.location.search).get("category");
 const container = document.querySelector(".product_card_container");
 const endpoint = `https://dummyjson.com/products/category/?category=${urlcategory}`;
-
+document.querySelector(".category_title").textContent = urlcategory;
 console.log(endpoint);
