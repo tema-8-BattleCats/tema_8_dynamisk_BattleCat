13a3420d05f99b0172210f4b7d2e0e9b06fba29f
Camille <caod0001@stud.ek.dk>
2026-03-18 18:00:29 +0100

    færdig js på index og productlist

--- js/productlist.js
+++ js/productlist.js
@@ -1,5 +1,57 @@
 const urlcategory = new URLSearchParams(window.location.search).get("category");
 const container = document.querySelector(".product_card_container");
-const endpoint = `https://dummyjson.com/products/category/?category=${urlcategory}`;
+const endpoint = `https://dummyjson.com/products/category/${urlcategory}`;
 document.querySelector(".category_title").textContent = urlcategory;
-console.log(endpoint);
+
+function getData() {
+  fetch(endpoint)
+    .then((respons) => respons.json())
+    .then(showData);
+}
+
+function showData(data) {
+  console.log(data);
+  let markup = "";
+  data.products.forEach((product) => {
+    markup += `<a href="productdetails.html?product=${product.id}">
+        <article class="product_card">
+          <div class="thumbnail">
+            <img
+              src="${product.thumbnail}"
+              alt="iPhone 20 Plus"
+            />
+          </div>
+
+          <div>
+            <h2 class="product_name">${product.title}</h2>
+            <p class="brand"><span>${product.brand}</span></p>
+            <!-- OBS fra H3 til P i brandnavn jf. prototype -->
+            <div class="rating_container">
+              <img
+                class="rating"
+                src="http://localhost:3845/assets/006fc72cc8a5bd479fa6ddcf1096de6687b79a40.svg"
+                alt="rating"
+              />
+              <p class="reviews">(${product.reviews.length} reviews)</p>
+            </div>
+          </div>
+
+          <p class="product_text">
+            ${product.description}
+          </p>
+          <br />
+          <div class="price">
+            <p>From <span>${product.price}</span>,-</p>
+          </div>
+          <br />
+          <div class="buttons">
+            <a class="primary_btn btn" href="/">Buy</a>
+            <a class="secondary_btn btn" href="/">Read more</a>
+          </div>
+        </article>
+        </a>`;
+  });
+  container.innerHTML = markup;
+}
+
+getData();
